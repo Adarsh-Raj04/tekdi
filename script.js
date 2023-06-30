eafc02cbd57b5f0d58e9f72ff78c974f8188e787
Adarsh Raj <77489636+Adarsh-Raj04@users.noreply.github.com>
2023-06-30 20:42:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,55 @@
-// Smooth scrolling when clicking on navbar links
-$(document).ready(function() {
-    $('#navbar a').on('click', function(event) {
-      if (this.hash !== '') {
-        event.preventDefault();
-  
-        const hash = this.hash;
-  
-        $('html, body').animate(
-          {
-            scrollTop: $(hash).offset().top
-          },
-          800 // Adjust the scrolling speed as desired
-        );
-      }
+document.querySelectorAll('a[href^="."]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
     });
   });
-  
+});
+
+function validateForm() {
+  var name = document.getElementById('name').value;
+  var number = document.getElementById('number').value;
+  var email = document.getElementById('email').value;
+  var message = document.getElementById('message').value;
+
+  var nameRegex = /^[a-zA-Z\s]+$/;
+  var numberRegex = /^\d+$/;
+  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  var isValid = true;
+
+  if (!nameRegex.test(name)) {
+    document.getElementById('name-error').innerHTML = 'Please enter a valid name';
+    isValid = false;
+  } else {
+    document.getElementById('name-error').innerHTML = '';
+  }
+
+  if (!numberRegex.test(number)) {
+    document.getElementById('number-error').innerHTML = 'Please enter a valid number';
+    isValid = false;
+  } else {
+    document.getElementById('number-error').innerHTML = '';
+  }
+
+  if (!emailRegex.test(email)) {
+    document.getElementById('email-error').innerHTML = 'Please enter a valid email';
+    isValid = false;
+  } else {
+    document.getElementById('email-error').innerHTML = '';
+  }
+
+  if (message === '') {
+    document.getElementById('message-error').innerHTML = 'Please enter a message';
+    isValid = false;
+  } else {
+    document.getElementById('message-error').innerHTML = '';
+  }
+
+  if (isValid) {
+    document.getElementById('success-message').innerHTML = 'Form submitted successfully!';
+    document.getElementById('form').reset();
+  }
+}
